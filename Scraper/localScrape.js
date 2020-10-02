9a731f693cb466bb19c0db449fc12b06cc10588e
BarrieLAJ <alhajiab.barrie@gmail.com>
2020-10-02 13:54:21 +0000

    Using try catch block to fix err

--- Scraper/localScrape.js
+++ Scraper/localScrape.js
@@ -1,88 +1,123 @@
-const puppetter = require('puppeteer');
-const path = require('path')
-const fs = require('fs')
-const LocalNews = require('../models/localNews');
-
+const puppetter = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+const LocalNews = require("../models/localNews");
 
 let previousNews;
 
 //localnews scraping
 const localScrape = async () => {
-    const browser = await puppetter.launch({headless:true,args:['--no-sandbox']});
+  try {
+    const browser = await puppetter.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
     const page = await browser.newPage();
-    const url = 'https://www.thesierraleonetelegraph.com/'
-    await page.goto('https://www.thesierraleonetelegraph.com/', {timeout: 0, waitUntil: 'networkidle2'});
-
-    
+    // const url = 'https://www.thesierraleonetelegraph.com/'
+    await page.goto("https://www.thesierraleonetelegraph.com/", {
+      timeout: 0,
+      waitUntil: "networkidle2",
+    });
 
     const localNews = await page.evaluate(() => {
-        //initializing varialbe to store the news that will be returned
-        const news = []
-            //dom query for the sections of the page
-        document.querySelectorAll('.mh-content .mh_magazine_posts_grid .mh-posts-grid').forEach((el) => {
-            //dom query for the articles in the each section
-            el.querySelectorAll('.mh-posts-grid-col').forEach((elem) => {
-                //setting values from the dom to the varialbes
-                let title = elem.querySelector('article.mh-posts-grid-item h3.entry-title a').textContent.trim()
-                let link = elem.querySelector('article.mh-posts-grid-item h3.entry-title a').getAttribute('href')
-                let img = elem.querySelector('article.mh-posts-grid-item figure.mh-posts-grid-thumb a.mh-thumb-icon img').getAttribute('src')
-                let category = elem.querySelector('article.mh-posts-grid-item figure.mh-posts-grid-thumb div.mh-image-caption').textContent.trim()
-                let body = elem.querySelector('article.mh-posts-grid-item div.mh-posts-grid-excerpt div.mh-excerpt p').textContent.trim()
-                let date = elem.querySelector('article.mh-posts-grid-item div.entry-meta span.entry-meta-date a').textContent
-                //adding each article to the news array
-                news.push({title,link,img,category,body,date})
-            })
-        })
-        //return all the news from the page
-        return news
-    })
+      //initializing varialbe to store the news that will be returned
+      const news = [];
+      //dom query for the sections of the page
+      document
+        .querySelectorAll(".mh-content .mh_magazine_posts_grid .mh-posts-grid")
+        .forEach((el) => {
+          //dom query for the articles in the each section
+          el.querySelectorAll(".mh-posts-grid-col").forEach((elem) => {
+            //setting values from the dom to the varialbes
+            let title = elem
+              .querySelector("article.mh-posts-grid-item h3.entry-title a")
+              .textContent.trim();
+            let link = elem
+              .querySelector("article.mh-posts-grid-item h3.entry-title a")
+              .getAttribute("href");
+            let img = elem
+              .querySelector(
+                "article.mh-posts-grid-item figure.mh-posts-grid-thumb a.mh-thumb-icon img"
+              )
+              .getAttribute("src");
+            let category = elem
+              .querySelector(
+                "article.mh-posts-grid-item figure.mh-posts-grid-thumb div.mh-image-caption"
+              )
+              .textContent.trim();
+            let body = elem
+              .querySelector(
+                "article.mh-posts-grid-item div.mh-posts-grid-excerpt div.mh-excerpt p"
+              )
+              .textContent.trim();
+            let date = elem.querySelector(
+              "article.mh-posts-grid-item div.entry-meta span.entry-meta-date a"
+            ).textContent;
+            //adding each article to the news array
+            news.push({ title, link, img, category, body, date });
+          });
+        });
+      //return all the news from the page
+      return news;
+    });
 
-
-if(fs.existsSync(path.join(__dirname,'localNews.json'))){
-    let newNews = []
-    fs.readFile(path.join(__dirname,'localNews.json'), (err,data) => {
-        if(err){
-            console.log(err)
-        }else{
-            previousNews = JSON.parse(data);
-            console.log(previousNews);
-        } 
-    } )
-    for(let news of localNews){
-        if(!previousNews.some(elem => elem.link == news.link)){
-            await LocalNews.create(news)
-            .then((data  => {
-                newNews.push(data)
-            })).catch(err => {
-                console.log(err)
-            })
+    if (fs.existsSync(path.join(__dirname, "localNews.json"))) {
+      let newNews = [];
+      fs.readFile(path.join(__dirname, "localNews.json"), (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          previousNews = JSON.parse(data);
+          console.log(previousNews);
         }
-}
-    fs.writeFile(path.join(__dirname,'localNews.json'), JSON.stringify(newNews), err => {
-        if(err) console.log(err);
-    })
-}else{
-    let newNews = []
-    for(let news of localNews){
+      });
+      for (let news of localNews) {
+        if (!previousNews.some((elem) => elem.link == news.link)) {
+          await LocalNews.create(news)
+            .then((data) => {
+              newNews.push(data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      }
+      fs.writeFile(
+        path.join(__dirname, "localNews.json"),
+        JSON.stringify(newNews),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    } else {
+      let newNews = [];
+      for (let news of localNews) {
         await LocalNews.create(news)
-         .then((data  => {
-            newNews.push(data)
-        })).catch(err => {
-            console.log(err)
-        })
+          .then((data) => {
+            newNews.push(data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      fs.writeFile(
+        path.join(__dirname, "localNews.json"),
+        JSON.stringify(newNews),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
     }
-    fs.writeFile(path.join(__dirname,'localNews.json'), JSON.stringify(newNews), err => {
-        if(err) console.log(err);
-    })
-}
 
+    //console.log(previousNews)
+    //console.log(localNews);
+    await browser.close();
+  } catch (error) {
+      return error
+  }
+};
 
-//console.log(previousNews)
-//console.log(localNews);
-await browser.close();
-}
-
- module.exports = localScrape
+module.exports = localScrape;
 // localScrape().then(()=>{
-    // console.log('Successfully scraped the website')
+// console.log('Successfully scraped the website')
 // }).catch((err)=>console.log(err));
--- index.js
+++ index.js
@@ -1,84 +1,86 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const localScrape = require('./Scraper/localScrape')
-const interScrape = require('./Scraper/interScrape')
-
+const localScrape = require("./Scraper/localScrape");
+const interScrape = require("./Scraper/interScrape");
 
 //connect to mongoDB
-mongoose.connect('mongodb+srv://alhaji_b:alhaji@1999@@sabisalonenews.vzeuw.gcp.mongodb.net/news?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(res=> console.log("DB Connected successfully"))
-
+mongoose
+  .connect(
+    "mongodb+srv://alhaji_b:alhaji@1999@@sabisalonenews.vzeuw.gcp.mongodb.net/news?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((res) => console.log("DB Connected successfully"));
 
 // scrapering
-const doScraping = async () =>{
-
-  await localScrape().then(()=>{console.log("Scraping local news Was Successful")})
-          .catch(err => {
-            console.log(`Error Scraping local Site ${err}`)
-          })
-
-  await interScrape().then(()=>{console.log("Scraping International news Was Successful")})
-    .catch(err => {
-      console.log(`Error Scraping international Site ${err}`)
+const doScraping = async () => {
+  await localScrape()
+    .then(() => {
+      console.log("Scraping local news Was Successful");
     })
+    .catch((err) => {
+      console.log(`Error Scraping local Site ${err}`);
+    });
 
-}
+  await interScrape()
+    .then(() => {
+      console.log("Scraping International news Was Successful");
+    })
+    .catch((err) => {
+      console.log(`Error Scraping international Site ${err}`);
+    });
+};
 
 //setting interval for scraping
 setInterval(async () => {
-   doScraping()
-        .then(() => {console.log("Scraping Done")} )
-        .catch(err => {console.log(`There was an error ${err}`)})
-}, 1000)
+  doScraping()
+    .then(() => {
+      console.log("Scraping Done");
+    })
+    .catch((err) => {
+      console.log(`There was an error ${err}`);
+    });
+}, 1000);
 
 // 1800000
 
 //routes
-const indexRoute = require('./routes/indexRout.routes')
-const apiRoutes = require('./routes/api.routes');
+const indexRoute = require("./routes/indexRout.routes");
+const apiRoutes = require("./routes/api.routes");
 
-
-const PORT = process.env.port || 3000
-
-
+const PORT = process.env.port || 3000;
 
 //middlewares
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //setting routes
-app.get('/',(req,res) =>{
-  res.json({ok: req.header})
-})
-app.use('/api/news', apiRoutes)
-
-
+app.get("/", (req, res) => {
+  res.json({ ok: req.header });
+});
+app.use("/api/news", apiRoutes);
 
 //error handling middlewares
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   // console.log(err)
-  res.status(412).send({errMsg: err.message})
-  next()
-})
-
+  res.status(412).send({ errMsg: err.message });
+  next();
+});
 
 //update database after get request
 // app.use((req,res,next)=>{
-  
+
 //    if(req.method === "GET" && req.url == "/api/news/local"){
 //       console.log('Entering scraper...')
 //       let num = 0
 //       if(!(num <= 10)){
 //         num++
 //       }else if(num == 10){
-        
+
 //       }
-      
+
 //  }
 //  else if(req.method === "GET" && req.url === "/api/news/international"){
 //    console.log('Entering International Scraper')
@@ -86,20 +88,16 @@ app.use((err,req,res,next)=>{
 //    if(!(num <= 10)){
 //     num++
 //   }else if(num == 10){
-    
+
 //   }
 // }
 
 // next();
 // })
 
-
-
 app.listen(PORT, () => {
-  console.log('Innitializing.....')
-  console.log('Starting the server....')
-  console.log('Server started..')
-  console.log('Server Listening on port 4000')
-})
-
-
+  console.log("Innitializing.....");
+  console.log("Starting the server....");
+  console.log("Server started..");
+  console.log("Server Listening on port 4000");
+});
